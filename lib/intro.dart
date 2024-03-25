124fa217423fd1b8c304976a4d05599f93f97e00
Vanessa1410 <110601743+Vanessa1410@users.noreply.github.com>
2024-03-25 21:09:03 +0530

    Update intro.dart

--- lib/intro.dart
+++ lib/intro.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:intro_slider/intro_slider.dart';
 import 'ind.dart';
 
 
